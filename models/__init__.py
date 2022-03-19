a7be5499ceff6d24093f6dcde928f11d14f11198
pericles001 <periclesadjovi@outlook.fr>
2022-03-20 00:20:13 +0100

    Cleanup code

--- models/__init__.py
+++ models/__init__.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+from os import environ
+
 from models.engine.db_storage import DBStorage
 
+from models.amenity import Amenity
 from models.base_model import BaseModel
 from models.city import City
+from models.engine.file_storage import FileStorage
+from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-from models.place import Place
-from models.amenity import Amenity
-from os import environ
 
 dummy_classes = {"BaseModel": BaseModel, "User": User,
                  "Review": Review, "City": City,
