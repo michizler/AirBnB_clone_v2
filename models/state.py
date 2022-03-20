974fd833cf3fd6f1e3bcb5f79e24cd62c232a6a5
pericles001 <periclesadjovi@outlook.fr>
2022-03-20 01:17:28 +0100

    Set state model up

--- models/state.py
+++ models/state.py
@@ -1,8 +1,32 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from os import environ
+
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
+import models
+from models.base_model import BaseModel, Base
+
+storage_engine = environ.get("HBNB_TYPE_STORAGE")
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    if storage_engine == "db":
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            """
+            cities list
+            """
+            result = []
+            for j, i in models.storage.all(models.city.City).items():
+                if i.state_id == self.id:
+                    result.append(i)
+            return result
