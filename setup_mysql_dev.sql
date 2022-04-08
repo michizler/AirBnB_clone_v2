a1646ad46225b87af02d10604d1b3e6a805fb7f5
pericles001 <periclesadjovi@outlook.fr>
2022-04-08 02:15:11 +0100

    Set dev database up

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,9 @@
 -- Creates a database called hbnb_dev_db in the current MySQL server
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- creates the MySQL server user hbnb_test
-CREATE USER IF NOT EXISTS 'hbnb_dev@localhost' IDENTIFIED BY 'hbnb_dev@pwd';
 -- grant permissions for user hbnb_test
-GRANT ALL ON `hbnb_dev_db`.* TO 'hbnb_dev@localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev@localhost';
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev@localhost' IDENTIFIED BY 'hbnb_dev@pwd';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev@localhost' IDENTIFIED BY 'hbnb_dev@pwd';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev@localhost' IDENTIFIED BY 'hbnb_dev@pwd';
+FLUSH PRIVILEGES;
