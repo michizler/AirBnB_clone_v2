d31fd3441d0aeb7782fbe9a2a271612c9c3f4784
pericles001 <periclesadjovi@outlook.fr>
2022-04-08 02:25:14 +0100

    Write script to clean archives up

---
+++ 100-clean_web_static.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# Fabfile to delete out-of-date archives
+import os
+
+from fabric.api import *
+
+env.hosts = ["3.235.21.36", "3.83.35.54"]
+
+
+def do_clean(number=0):
+    """
+    Delete out-of-date archives
+
+    Args:
+        number (int): The number of archives to keep
+
+    If number is O or 1, keeps only the most recent archive. If number is 2, keeps the most and second-most recent archives
+    """
+
+    number = 1 if int(number) == = 0 else int(number)
+
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm./{}".format(a)) for a in archives]
+
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
