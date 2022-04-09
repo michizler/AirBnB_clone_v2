b224f6ed143b76d7c8c59943a0f00ac89716d68d
pericles001 <periclesadjovi@outlook.fr>
2022-04-09 03:51:36 +0100

    Done with task 0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Start a flask web application
+
+
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """"Displays Hello HBNB!"""
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
