9a11cdd0ed563e1a400bd089a4cdd264d53e14c1
pericles001 <periclesadjovi@outlook.fr>
2022-04-09 04:14:11 +0100

    Launch app for test

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -43,3 +43,7 @@ def python(text='is cool'):
     """Display 'Python ' followed by the value of
     the text variable (replace underscore _ symbols with a space )"""
     return 'Python ' + text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
